714a58776a4deb946ccd50f1410441db0499973f
jrandiny <jrandiny2@gmail.com>
2018-08-09 21:24:36 +0700

    Add map
    credit
    - @naufalzhafran

--- App.js
+++ App.js
@@ -11,26 +11,44 @@ const panjangLayar = Dimensions.get('window').height;
 export default class App extends React.Component {
   render() {
     return (
-        <View style={styles.container}>
-            <View style={[styles.searchBar,styles.elevated]}>
-                <TextInput
-                  style={styles.searchInput}
-                  placeholder="Search here"
-                />
-          </View>
-
-          <TouchableOpacity onPress={()=>{Alert.alert("test")}} style={[styles.fab,styles.elevated]}>
-              <Image
-                style={[styles.searchLogo]}
-                source={{uri:'https://png.icons8.com/metro/1600/search.png'}}
-              />
-          </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={[styles.searchBar,styles.elevated]}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search here"
+          />
         </View>
+
+        <ImageZoom
+          cropWidth  = {lebarLayar}
+          cropHeight = {panjangLayar}
+          imageWidth = {500}
+          imageHeight = {1000}
+          minScale = {1}
+          enableCenterFocus = {false}
+          style = {styles.mapArea}>
+          <Image style={{width:500, height:1000}}
+            source={{uri:'https://picsum.photos/500/1000'}}/>
+        </ImageZoom>
+
+        <TouchableOpacity onPress={()=>{Alert.alert("test")}} style={[styles.fab,styles.elevated]}>
+          <Image
+            style={[styles.searchLogo]}
+            source={{uri:'https://png.icons8.com/metro/1600/search.png'}}
+          />
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  mapArea:{
+    position:"absolute",
+    top:0,
+    left:0,
+    zIndex:-999
+  },
   container: {
     flex: 1,
     backgroundColor: myColor.appBg,
